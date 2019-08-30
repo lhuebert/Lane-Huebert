94632287cc0c2f9a3b3f455d7e043f91adf4090a
Lane Huebert <lhuebert@huskers.unl.edu>
2019-08-29 20:25:43 -0500

    Initial commit of Github demo

---
+++ hello.c
@@ -0,0 +1,16 @@
+/**
+  Author: Lane Huebert
+  Date: 2019/08/29
+
+  Github demo
+
+  */
+#include <stdlib.h>
+#include <stdio.h>
+
+int main (int argc, char **argv) {
+
+  printf("Lane Huebert\n");
+
+  return 0;
+}
